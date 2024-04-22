bfa055b2334f016a09ac6bb5464e04ea68ddca99
AnjasFedo <fedoaf98@gmail.com>
2024-04-22 23:25:55 +0700

    Firebase Configuration

--- src/schemas/schema.ts
+++ src/schemas/schema.ts
@@ -1,4 +1,3 @@
-import { password } from "bun";
 import z from "zod";
 
 export const envSchema = z.object({
@@ -7,6 +6,12 @@ export const envSchema = z.object({
   REDIS_URL: z.string(),
   SESSION_SECRET: z.string(),
   TOKEN_SECRET: z.string(),
+  API_KEY: z.string(),
+  AUTH_DOMAIN: z.string(),
+  PROJECT_ID: z.string(),
+  STORAGE_BUCKET: z.string(),
+  MESSAGING_SENDER_ID: z.string(),
+  APP_ID: z.string(),
 });
 
 export const userSchema = z.object({
